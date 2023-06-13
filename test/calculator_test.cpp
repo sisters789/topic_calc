5b60e8a33e604aad518a455db056825f5a880719
gb <nguyengiabao442@gmail.com>
2023-06-13 08:30:12 +0000

    update CMakeLists.txt for testing

--- test/calculator_test.cpp
+++ test/calculator_test.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "calculator.h"
+
+using namespace std;
+
+TEST(calcsuite, calcname)
+{
+
+}
+
 int main(int argc, char** argv)
 {
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
--- test/scientific_test.cpp
+++ test/scientific_test.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+#include <gtest/gtest.h>
 
+#include "scientific.h"
+
+using namespace std;
+
+TEST(scisuite, sciname)
+{
+
+}
 
 int main(int argc, char** argv)
 {
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
