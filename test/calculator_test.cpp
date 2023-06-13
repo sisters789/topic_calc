4b8eb98d3c81810f880f1c3b4414a4817baf0b37
gb <nguyengiabao442@gmail.com>
2023-06-13 11:03:48 +0000

    update test cases in calculator_test.cpp change position of enable_testing()

--- test/calculator_test.cpp
+++ test/calculator_test.cpp
@@ -1,13 +1,60 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <sstream>
 
 #include "calculator.h"
 
 using namespace std;
 
-TEST(calcsuite, calcname)
+class CalculatorTest : public ::testing::Test
 {
+    protected:
+        Calculator calc;
+};
 
+TEST_F(CalculatorTest, InitClassTest)
+{
+    ASSERT_ANY_THROW(calc.parseInput("abc"));
+}
+
+TEST_F(CalculatorTest, NumberParseInputTest)
+{
+    EXPECT_EQ(calc.parseInput("-10"), -10);
+    EXPECT_EQ(calc.parseInput("-10.1"), -10.1);
+    EXPECT_EQ(calc.parseInput("0"), 0);
+    EXPECT_EQ(calc.parseInput("10"), 10);
+    EXPECT_EQ(calc.parseInput("10.1"), 10.1);
+}
+
+TEST_F(CalculatorTest, ValidStringParseInputTest)
+{
+    ASSERT_EQ(calc.parseInput("result"), 0);
+    ASSERT_EQ(calc.parseInput("mem"), 0);
+}
+
+TEST_F(CalculatorTest, InvalidStringParseInputTest)
+{
+    ASSERT_ANY_THROW(calc.parseInput("abc"));
+}
+
+TEST_F(CalculatorTest, PrintMemTest)
+{
+    ::testing::internal::CaptureStdout();
+    calc.printMem();
+    string output = ::testing::internal::GetCapturedStdout();
+    string expected = "The mem is 0";
+
+    EXPECT_EQ(output, expected);
+}
+
+TEST_F(CalculatorTest, SetMemTest)
+{
+    ::testing::internal::CaptureStdout();
+    calc.printMem();
+    string output = ::testing::internal::GetCapturedStdout();
+    string expected = "The mem is 0";
+
+    EXPECT_EQ(output, expected);
 }
 
 int main(int argc, char** argv)
